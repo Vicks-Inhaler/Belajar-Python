1492eea625f38570609b3b84dd908e022823e048
Vicks-Inhaler <agung.firdaus766@gmail.com>
2025-05-07 16:01:30 +0700

    Program Operator Aritmatika

---
+++ OperatorAritmatika.py
@@ -0,0 +1,37 @@
+Friends = 0
+Friends1 = 0
+Teman = 5
+Teman1 = 5
+Teman2 = 5
+Teman3 = 5
+Teman4 = 5
+Teman5 = 5
+Teman6 = 5
+Teman7 = 5
+Konco = 10
+
+Friends = Friends + 1
+Friends1 += 1
+Teman = Teman - 2
+Teman1 -= 2
+Teman2 = Teman * 3
+Teman3 *= 3
+Teman4 = Teman / 2
+Teman5 /= 2
+Teman6 = Teman ** 2
+Teman7 **= 2
+Remainder = Konco % 2
+Remainder1 = Konco % 3
+
+print(Friends)
+print(Friends1)
+print(Teman)
+print(Teman1)
+print(Teman2)
+print(Teman3)
+print(Teman4)
+print(Teman5)
+print(Teman6)
+print(Teman7)
+print(Remainder)
+print(Remainder1)
