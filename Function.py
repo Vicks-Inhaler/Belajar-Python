c957cdd1c72eee26b3dce7f6b4bd6f4c778504a9
Vicks-Inhaler <agung.firdaus766@gmail.com>
2025-05-07 16:26:48 +0700

    Program Function

---
+++ Function.py
@@ -0,0 +1,16 @@
+X = 3.14
+Y = 4
+Y1 = -4
+Z = 5
+
+Result = round(X) # Pembulatan Nilai di Belakang Koma
+Hasil = abs(Y1) # Nilai Positif
+Hitung = pow(4, 3) # Hitung 4 pangkat 3
+Maximum = max(X, Y, Z) # Nilai Paling Besar
+Minimum = min(X, Y, Z) # Nilai Paling Kecil
+
+print(Result)
+print(Hasil)
+print(Hitung)
+print(Maximum)
+print(Minimum)
