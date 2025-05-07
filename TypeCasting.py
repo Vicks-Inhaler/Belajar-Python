4859e83e3f6482c261df7ed2341c192485f1d461
Vicks-Inhaler <agung.firdaus766@gmail.com>
2025-05-07 16:03:48 +0700

    Program Type Casting

---
+++ TypeCasting.py
@@ -0,0 +1,26 @@
+# TypeCasing = The process of converting a variable from one data type to another
+#              str(), int(), float(), bool()
+
+#=======================================================
+Name = "Kadal"
+Jeneng = ""
+Age = "25"
+Ages = 25
+GPA = 3.3
+Student = True
+
+Age = str(Age)
+Ages = int(Ages)
+GPA = float(GPA)
+Student = bool(Student)
+Pelajar = bool(Jeneng)
+
+Age += "1"
+Ages += 1
+
+print(Name)
+print(Age)
+print(Ages)
+print(GPA)
+print(Student)
+print(Pelajar)
