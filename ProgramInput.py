08c48813f917b258f559252e55ab8672d11d59b7
Vicks-Inhaler <agung.firdaus766@gmail.com>
2025-05-07 16:07:20 +0700

    Program Input Data dan Hitung

---
+++ ProgramInput.py
@@ -0,0 +1,20 @@
+# Input() = A function that prompts the user to enter data
+#           Return the entered data as a string
+
+input("What is Your Name? : ")
+Name = input("What is Your Name? : ")
+Age = input("How Old Are You? : ")
+Usia = int(input("Berapa Umurmu? : "))
+
+Umur = int(Age)
+Umur = Umur + 1
+Usia = Usia + 1
+
+print(f"Hello {Name}!")
+print("HAPPY BIRTHDAY!")
+print(f"You Are {Age} Years Old")
+print(f"Umur Kamu {Umur} Tahun")
+print(f"Usiamu {Usia} Tahun")
+
+# Note :
+# f = untuk String
