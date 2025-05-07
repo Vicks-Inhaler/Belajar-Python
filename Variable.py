232f8181e903d574fa3ae2608adbacf8103b6257
Vicks-Inhaler <agung.firdaus766@gmail.com>
2025-05-07 16:02:47 +0700

    Program Variable

---
+++ Variable.py
@@ -0,0 +1,93 @@
+# Variable = A container for a value (string, integer, float, boolean)
+#            A variable behaves as if it was the value it contains
+
+#===========================================
+# String
+First_Name = "Kadal"
+Food = "Nyambik"
+Email = "KadalMesir@gmail.com"
+
+# Integer
+Age = 25
+Quantity = 3
+Num_Of_Students = 30
+
+# Float
+Price = 10.99
+Harga = 10000
+GPA = 3.2
+Distance = 5.5
+
+# Boolean
+# 1
+Student = True
+# 2
+Is_Student = False
+# 3
+Sale = True
+# 4
+For_Sale = False
+# 5
+Online = True
+# 6
+Is_Online = False
+
+# Cetak String
+print(First_Name)
+print("First_Name")
+print(f"Hello {First_Name}")
+print(f"You Like {Food}")
+print(f"You Email is : {Email}")
+
+# Cetak Integer
+print(f"Your Are {Age} Years Old")
+print(f"Your Are Buying {Quantity} Items")
+print(f"Your Class Has {Num_Of_Students} Students")
+
+# Cetak Float
+print(f"The Price Is ${Price}")
+print(f"The Price Is Rp. {Harga}")
+print(f"Your GPA Is {GPA}")
+print(f"Your Ran {Distance} KM")
+
+# Cetak Boolean
+print(f"Are You A Student?: {Student}")
+
+# Logic Boolean
+# 1
+if Student :
+    print("You Are A Student")
+else :
+    print("You Are Not A Student")
+
+# 2
+if Is_Student :
+    print("You Are A Student")
+else :
+    print("You Are Not A Student")
+
+# 3
+if Sale :
+    print("That Item Is For Sale")
+else :
+    print("That Item Is Not Available")
+
+# 4
+if For_Sale :
+    print("That Item Is For Sale")
+else :
+    print("That Item Is Not Available")
+
+# 5
+if Online :
+    print("You Are Online")
+else :
+    print("You Are Offline")
+
+# 6
+if Is_Online :
+    print("You Are Online")
+else :
+    print("You Are Offline")
+
+#===========================================
