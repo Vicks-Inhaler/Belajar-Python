8e14ab55c3336ab02821364f0ae72a29c5fb5a26
Vicks-Inhaler <agung.firdaus766@gmail.com>
2025-05-07 16:09:20 +0700

    Program Permainan Merangkai Kata

---
+++ MadLibsGame.py
@@ -0,0 +1,14 @@
+# MadLibs Game
+# Word Game Where You Create A Story
+# By Filling In Blanks With Random Words
+
+Adjective1 = input("Enter An Adjective (Description) : ")
+Noun1 = input("Enter A Noun (Person, Place, Thing) : ")
+Adjective2 = input("Enter An Adjective (Description) : ")
+Verb1 = input("Enter A Verb Ending With 'ing' : ")
+Adjective3 = input("Enter An Adjective (Description) : ")
+
+print(f"Today I Went To A {Adjective1} Zoo.")
+print(f"In An Exhibit, I Saw A {Noun1}")
+print(f"{Noun1} Was {Adjective2} And {Verb1}")
+print(f"I Was {Adjective3}!")
