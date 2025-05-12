b44f3a55b3a4b4d5118b0b8eebdfbbd697f91612
Vicks-Inhaler <agung.firdaus766@gmail.com>
2025-05-12 17:19:29 +0700

    Program Math Modules (import math, import math as m, from math import pi, from math import e)

---
+++ MathModules.py
@@ -0,0 +1,35 @@
+# Module = A file containing code you want to include in your program
+#          Use 'import' to include a module (built-in or your own)
+#          Useful to break up a large program reusable separate files
+
+# print(help("modules"))
+# print(help("math"))
+
+print()
+import math
+import math as m
+from math import pi
+from math import e
+
+print(math.pi)
+print(m.pi)
+print(pi)
+print(e)
+
+print()
+a, b, c, d, e = 1, 2, 3, 4, 5
+
+print(e ** a)
+print(e ** b)
+print(e ** c)
+print(e ** d)
+print(e ** e)
+
+print()
+a, b, c, d, e = 1, 2, 3, 4, 5
+
+print(math.e ** a)
+print(math.e ** b)
+print(math.e ** c)
+print(math.e ** d)
+print(math.e ** e)
